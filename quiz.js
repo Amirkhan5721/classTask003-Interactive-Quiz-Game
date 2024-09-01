62e27b280f249da2c294d934d10ccd04299d812b
Amirkhan5721 <amirkhanafridi959@gmail.com>
2024-09-01 10:40:16 -0700

    added

---
+++ quiz.js
@@ -0,0 +1,11 @@
+let correctAnwer;
+let question = [
+    {question:'What is the capital of Pakistan?'},
+    {answer: 'A)Islamabad, B)Peshawar, C)Karachi, D)Multan,'
+    ,correctAnwer:"A"}
+];
+
+function clickableBtn(){
+}
+clickableBtn();
+
